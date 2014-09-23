aa53a8b96826bbfc02b7f15a8720ae1659ae4f69
Jason Montleon <jmontleo@redhat.com>
2014-09-23 10:37:34 -0400

    add ruby193-rubygem-passenger* packages

--- centos/6/foreman/additions.sh
+++ centos/6/foreman/additions.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-yum install -y foreman-sqlite foreman-mysql2 foreman-postgresql foreman-compute foreman-gce foreman-libvirt foreman-ovirt foreman-vmware mod_passenger
+yum install -y foreman-sqlite foreman-mysql2 foreman-postgresql foreman-compute foreman-gce foreman-libvirt foreman-ovirt foreman-vmware mod_passenger ruby193-rubygem-passenger-native
